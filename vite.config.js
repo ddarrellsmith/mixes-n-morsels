2abcf97eb8a50495e4effdffdf197c69e0e25cd1
Darrell <ddarrellsmith@gmail.com>
2026-07-14 18:51:04 -0500

    Deploy built files

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/mixes-n-morsels/",
+  base: "./",
   plugins: [react()],
+  build: {
+    outDir: "docs",
+  },
 });
